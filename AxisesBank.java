4916b11cee5925d518db513608ca19790e8d90c6
rpushaishwarya <97538417+rpushaishwarya@users.noreply.github.com>
2022-11-02 20:01:03 +0530

    Add files via upload

---
+++ AxisesBank.java
@@ -0,0 +1,14 @@
+package week4.day3;
+
+public class AxisesBank extends BankInFo {
+	public void deposite() {
+		System.out.println("1000");
+	}
+	public static void main(String[] args) {
+		 AxisesBank ax=new  AxisesBank();
+		 ax.deposite();
+		 
+		
+	}
+
+}
---
+++ BankInFo.java
@@ -0,0 +1,15 @@
+package week4.day3;
+//example for overriding
+
+public class BankInFo {
+	public void saving() {
+		System.out.println("500");
+	}
+	public void fixed() {
+		System.out.println("1000");
+	}
+	public void deposite() {
+		System.out.println("500");
+	}
+
+}
